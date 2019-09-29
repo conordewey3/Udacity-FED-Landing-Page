d0612813e7a39476963529ec5e02d69a5843b51b
conordewey3 <cdewey96@vt.edu>
2019-09-28 22:53:51 -0400

    all functionality done

--- js/app.js
+++ js/app.js
@@ -26,9 +26,18 @@ const sections = document.querySelectorAll('section')
  * 
 */
 
-// listens for click on nav
-function scrollToClick(clicked) {
-    clicked.scrollIntoView();
+// check which element is active
+function getActiveElem() {
+    maxSection = sections[0];
+    minVal = 1000000;
+    for (item of sections) {
+        let bounding = item.getBoundingClientRect();
+        if (bounding.top > -300 & bounding.top < minVal) {
+            minVal = bounding.top;
+            maxSection = item;
+        };
+    };
+    return maxSection;
 };
 
 /**
@@ -49,13 +58,35 @@ function addSections() {
 };
 
 // Add class 'active' to section when near top of viewport
-
+function setActive () {
+    window.addEventListener('scroll', function (event) {
+        let section = getActiveElem();
+        section.classList.add('your-active-class');
+        // set other sections as inactive
+        for (let item of sections) {
+            if (item.id != section.id & item.classList.contains('your-active-class')) {
+                item.classList.remove('your-active-class');
+            }
+        }
+        // set corresponding header style
+        const active = document.querySelector('li[data-nav="' + section.id + '"]');
+        active.classList.add('active__link');
+        // remove from other headers
+        const headers = document.querySelectorAll('.menu__link');
+        for (let item of headers) {
+            console.log(item);
+            if (item.dataset.nav != active.dataset.nav & item.classList.contains('active__link')) {
+                item.classList.remove('active__link');
+            }
+        };
+    });
+};
 
 // Scroll to anchor ID using scrollTO event
-function listenForClicks() {
+function scrollToClick() {
     navbar.addEventListener('click', function (event) {
         const clicked = document.querySelector('#' + event.target.dataset.nav)
-        scrollToClick(clicked);
+        clicked.scrollIntoView();
     });
 };
 
@@ -70,8 +101,8 @@ function listenForClicks() {
 addSections();
 
 // Scroll to section on link click
-listenForClicks();
+scrollToClick();
 
 // Set sections as active
-
+setActive();
 
